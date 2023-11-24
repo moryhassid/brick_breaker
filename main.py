4750326d55f7e5767d79f75e8c0bb3ece61356bc
Mory_Hassid <mory9870@gmail.com>
2023-11-24 10:26:34 +0200

    I have added: 1. Gravity for the ball 2. Function for checking if ball hits the ground 3. Function for checking if ball hits racket

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from pygame import draw
 import pygame
+import math
 import random
 
 BACKGROUND_COLOR = (255, 255, 255)
@@ -10,11 +11,26 @@ BRICK_HEIGHT = 20
 BRICK_WIDTH = 42
 NUMBER_OF_BRICK_ROWS = 4
 NUMBER_OF_BRICKS_IN_ROW = 18
-
+RADIUS_SIZE = 15
 RACKET_HEIGHT = 10
 RACKET_WIDTH = 80
 
+
+def start_new_game():
+    game_screen = pygame.display.set_mode((WIDTH_SCREEN, HEIGHT_SCREEN))
+    center_ball_position = pygame.Vector2(game_screen.get_width() / 2, game_screen.get_height() / 2)
+    return center_ball_position
+
+
+def is_ball_hitting_the_racket(player_racket, current_ball_position):
+    # Here is the constraint for the x axis
+    # We are declaring on a ball position compared to racket position
+    result = player_racket.x + RADIUS_SIZE <= current_ball_position.x <= player_racket.x + RACKET_WIDTH - RADIUS_SIZE
+    return result
+
+
 if __name__ == '__main__':
+
     pygame.init()
     clock = pygame.time.Clock()
     # font = pygame.font.Font('fonts/ChrustyRock-ORLA.ttf', 32)
@@ -31,6 +47,8 @@ if __name__ == '__main__':
 
     # Initialization:
     racket = pygame.Rect(WIDTH_SCREEN / 2, HEIGHT_SCREEN - 20, RACKET_WIDTH, RACKET_HEIGHT)
+    ball_position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    step_y = 10
 
     while True:
         screen.fill(BACKGROUND_COLOR)
@@ -47,8 +65,23 @@ if __name__ == '__main__':
                           color=brick_color,
                           rect=brick)
 
-        disc_position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-        disc = pygame.draw.circle(screen, BALL_COLOR, disc_position, 15)
+        ball = pygame.draw.circle(screen, BALL_COLOR, ball_position, RADIUS_SIZE)
+
+        # Handle_in_case_ball_hitting_the_ground
+        if ball_position.y > HEIGHT_SCREEN - RADIUS_SIZE:
+            print("You have reached the ground")
+            ball_position = start_new_game()
+
+        if ball_position.y > HEIGHT_SCREEN - 20 - RADIUS_SIZE:
+            if is_ball_hitting_the_racket(player_racket=racket,
+                                          current_ball_position=ball_position):
+                # Changing direction of the ball
+                step_y *= -1
+                print('debug')
+
+        # only for starting
+        ball_position.x += 5  # random.randint(1, 5)
+        ball_position.y += step_y  # random.randint(1, 10)
 
         draw.rect(surface=screen,
                   color=RACKET_COLOR,
